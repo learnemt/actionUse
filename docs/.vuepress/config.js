26a1710b67988a19bb92aef05196a6f50768e7ba
learnemt <77092327+learnemt@users.noreply.github.com>
2022-09-12 21:53:31 +0800

    fix 1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,11 +13,12 @@ import {
 // import { gitPlugin } from '@vuepress/plugin-git'
 
 export default {
+  base: '/actionUse/',
   locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
     '/': {
-      base: '/actionUse/',
+
       lang: '简体中文',
       title: 'Demo',
       description: 'Vue 驱动的静态网站生成器',
